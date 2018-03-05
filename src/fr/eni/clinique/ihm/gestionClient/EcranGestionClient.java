7be78d4967f930c97a399e4019054744c65da096
SOULA MORGANE <morgane.soula2017@campus-eni.fr>
2018-03-05 16:13:30 +0100

    Start of the buttons client's display

--- src/fr/eni/clinique/ihm/gestionClient/EcranGestionClient.java
+++ src/fr/eni/clinique/ihm/gestionClient/EcranGestionClient.java
@@ -5,21 +5,28 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowAdapter;
 import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
+import javax.swing.WindowConstants;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 import fr.eni.clinique.bll.BLLException;
 import fr.eni.clinique.bo.Client;
+import fr.eni.clinique.bo.Personnel;
 import fr.eni.clinique.bo.Observable.Observer;
+import fr.eni.clinique.ihm.AppliTestIHM;
 import fr.eni.clinique.ihm.MainFrame;
 import javafx.scene.layout.Border;
 
@@ -36,6 +43,13 @@ public class EcranGestionClient extends JPanel implements Observer {
 	private JScrollPane scrollPane;
 	
 	private JPanel panelScrollTable;
+	private JPanel buttonForm;
+	
+	private BoutonMenuClient panelButtons;
+	
+	private JDialog creationView;
+	
+	private CreationClientPanel creationClientPanel;
 	
 	URL iconURL;
 	ImageIcon icon;
@@ -43,6 +57,7 @@ public class EcranGestionClient extends JPanel implements Observer {
 
 	public EcranGestionClient() {
 		super (new BorderLayout());
+		add(getButtonForm(), BorderLayout.NORTH);
         add(getPanelScrollTable(), BorderLayout.CENTER);
 		this.iconURL = MainFrame.class.getResource("ressources/ico_veto.png");
         this.icon = new ImageIcon(iconURL);
@@ -78,9 +93,13 @@ public class EcranGestionClient extends JPanel implements Observer {
 		getTableClient().setVisible(true);
 	}
 	
+	public Client getClientFromJTable(int row) {
+		Client client = this.model.getClient(row);
+		return client;
+	}
 	
 	
-	private JTable getTableClient() {
+	public JTable getTableClient() {
 		if(tableClient == null) {
 			try {
 				model = new ClientTableModel();
@@ -100,10 +119,44 @@ public class EcranGestionClient extends JPanel implements Observer {
 						} catch (ClientNotFoundException e) {
 							e.printStackTrace();
 						} catch (BLLException e) {
-							e.printStackTrace();
+							e.printStackTrace(); 
 						}
 					}
 				});
+					
+					tableClient.addMouseListener(new MouseListener() {
+	                    @Override
+	                    public void mouseClicked(MouseEvent e) {
+	                        if (e.getClickCount() == 2) {
+	                            JTable target = (JTable)e.getSource();
+	                            int row = target.getSelectedRow();
+	                            Client client = getClientFromJTable(row);
+	                            getCreationViewOnUpdate(client.getCodeClient());
+	                            e.consume();
+	                        }
+	                    }
+
+	                    @Override
+	                    public void mousePressed(MouseEvent e) {
+
+	                    }
+
+	                    @Override
+	                    public void mouseReleased(MouseEvent e) {
+
+	                    }
+
+	                    @Override
+	                    public void mouseEntered(MouseEvent e) {
+
+	                    }
+
+	                    @Override
+	                    public void mouseExited(MouseEvent e) {
+
+	                    }
+	                });
+
 			} catch (BLLException e) {
 				JOptionPane.showMessageDialog(this, "Erreur lors du chargement des données", "Erreur", JOptionPane.ERROR_MESSAGE);
 			}
@@ -133,6 +186,77 @@ public class EcranGestionClient extends JPanel implements Observer {
 		panel.add(component, gbc);
 	}
 	
+	private JPanel getButtonForm() {
+		if (buttonForm == null) {
+			buttonForm = new JPanel(new GridBagLayout());
+			
+			addComponentTo(getPanelButtons(), buttonForm, 0, 0, 2, 1, 1);
+		}
+		
+		return buttonForm;
+	}
+	
+	private BoutonMenuClient getPanelButtons() {
+		if (panelButtons == null) {
+			panelButtons = new BoutonMenuClient(this);
+		}
+		
+		return panelButtons;
+	}
+	
+	public JDialog getCreationView() {
+		this.creationView = new JDialog();
+		this.creationView.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		this.creationView.addWindowListener(new WindowAdapter() {
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				reloadView();
+				if (!creationClientPanel.isSaved()) {
+					int reply = JOptionPane.showConfirmDialog(getCreationClientPanel(), "Toutes modifications non enregistrées seront perdues !\nVoulez-vous vraiment quitter ?", "Quitter", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+					if (reply == JOptionPane.YES_OPTION) {
+						getCreationClientPanel().resetDialog();
+						creationView.setVisible(false);
+						reloadView();
+					}
+				} else {
+					getCreationClientPanel().resetDialog();
+					creationView.setVisible(false);
+					reloadView();
+				}
+				getTableClient().clearSelection();
+			}
+		});
+		
+		this.creationView.setContentPane(this.getCreationClientPanel());
+		this.creationView.setVisible(true);
+		this.creationView.setSize(400, 200);
+		this.creationView.setLocation(600, 250);
+		
+		this.creationView.setIconImage(icon.getImage());
+		this.creationView.setResizable(false);
+		this.creationView.setTitle("Création Client");
+		this.getCreationClientPanel().resetDialog();
+		
+		return creationView;
+	}
+	
+	public JDialog getCreationViewOnUpdate(int id) {
+		this.getCreationView();
+		this.creationView.setTitle("Mise à jour du client");
+		this.getCreationClientPanel().writeInputs(id);
+		
+		return creationView;
+	}
+	
+	
+	public CreationClientPanel getCreationClientPanel() {
+		if(this.creationClientPanel == null) {
+			this.creationClientPanel = new CreationClientPanel();
+		}
+		
+		return creationClientPanel;
+	}
 	
 	
 	public void onChanged(Object value) {
@@ -142,6 +266,15 @@ public class EcranGestionClient extends JPanel implements Observer {
 	
 	public void onCreate() {
 		
+	}
+	
+	public void reloadView() {
+		try {
+			this.model.updateData();
+		} catch (BLLException e) {
+			e.printStackTrace();
+			AppliTestIHM.showError("Erreur rechargement des données", "Erreur de mise à jour des données:\n" + e.getMessage());
+		}
 	}
 
 
